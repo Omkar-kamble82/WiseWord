b369078e34e47c6fea369c13a5b32eb712c66ac8
Omkar-kamble82 <omkarsk82@gmail.com>
2024-01-13 20:14:06 +0530

    chatbot

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,5 +1,5 @@
 import { generateReactHelpers } from "@uploadthing/react/hooks";
 
-import type { OurFileRouter } from "@/app/api/uploadthing/core";
+import type { OurFileRouter } from "@/app/(root)/api/uploadthing/core";
 
 export const { useUploadThing, uploadFiles } = generateReactHelpers<OurFileRouter>();
