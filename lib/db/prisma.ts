6e6900d651468dc3100534b7fee442bb48c921ed
Omkar-kamble82 <omkarsk82@gmail.com>
2024-01-13 20:40:44 +0530

    script fix

--- lib/db/prisma.ts
+++ lib/db/prisma.ts
@@ -1,15 +1,17 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-    return new PrismaClient()
-}
+    return new PrismaClient();
+};
 
-declare global {
-    var prisma: undefined | ReturnType<typeof prismaClientSingleton>
-}
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-const prisma = globalThis.prisma ?? prismaClientSingleton()
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClientSingleton | undefined;
+};
 
-export default prisma
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
