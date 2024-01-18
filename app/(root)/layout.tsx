9b32533b723ad5fefeffb406e8fed2b46b91e3ed
Omkar Kamble <96938880+Omkar-kamble82@users.noreply.github.com>
2024-01-18 19:54:26 +0530

    change

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -10,7 +10,6 @@ export default function RootLayout({
         <div className="">
             <Navbar />
             <main>{children}</main>
-            <span className="fixed bottom-6 right-6 "><Aibutton/></span>
         </div>
     )
 }
