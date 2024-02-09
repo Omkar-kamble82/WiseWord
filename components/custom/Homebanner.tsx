64d93ac4202a526bc3b4a4747843a9a29f3980da
Omkar Kamble <96938880+Omkar-kamble82@users.noreply.github.com>
2024-02-09 21:40:32 +0530

    final

--- components/custom/Homebanner.tsx
+++ components/custom/Homebanner.tsx
@@ -10,8 +10,8 @@ const Homebanner = (props: Props) => {
         <div className="min-h-[96vh] bg-slate-50 flex justify-center items-center flex-col px-5">
             <Image height={500} width={600} className="object-contain sm:h-[420px]" src={"/home-banner.svg"} alt={"Home-banner"}/>
             <span className="text-center max-w-7xl mx-auto">
-                <h1 className="text-[23px] sm:text-[40px] font-extrabold">WiseWord: The Art of Mindful Relationships Blog</h1>
-                <p className="text-[12px] sm:text-[16px] text-muted-foreground"> Embark on a cultural journey exploring timeless wisdom from different corners of the world. Uncover valuable insights, traditions, and philosophies that have stood the test of time, offering lessons for a more meaningful and interconnected existence.</p>
+                <h1 className="text-[23px] sm:text-[40px] font-extrabold">WiseWord: Elevate Your Reading Experience with AI-Enhanced Blogs</h1>
+                <p className="text-[12px] sm:text-[16px] text-muted-foreground">Embark on a cultural journey exploring timeless wisdom from different corners of the world. Uncover valuable insights, traditions, and philosophies that have stood the test of time, offering lessons for a more meaningful and interconnected existence.</p>
                 <Link href={"/sign-up"}><Button className="my-[8px] sm:text-[16px] p-6 font-semibold" variant={"default"}> Get started with WiseWord</Button></Link>
                 <p className="text-muted-foreground text-[15px]">*Already a member?  <Link href={"/sign-in"}><span className="underline cursor-pointer">SignIn</span></Link></p>
             </span>
